f1d490bb39035468e76a798974a3f14c0cc69857
mattellis91 <mathew.ellis2@griffithuni.edu.au>
2024-06-19 11:00:10 +1000

    added basic ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SidebarModule } from 'primeng/sidebar';
+import { SplitterModule } from 'primeng/splitter';
+import { TreeModule } from 'primeng/tree';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { EditorComponent } from './editor/editor.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    SplitterModule,
+    TreeModule,
+    TableModule,
+    ToolbarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
